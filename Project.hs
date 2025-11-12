5033a40d9ce30fba95a19048a373711a478c1f9a
Oaketree07 <oakeskyle502@gmail.com>
2025-11-12 01:51:17 +0000

    Added The Files For The Project

---
+++ Project.hs
@@ -0,0 +1,11 @@
+module Project ( prove, proveCount ) where
+
+import Sequent
+
+prove :: Sequent -> [Sequent]
+prove = undefined
+
+-- for challenge part
+
+proveCount :: Sequent -> ([Sequent],Int)
+proveCount = undefined
