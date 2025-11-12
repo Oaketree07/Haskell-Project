733e83909b949ade1e085c823c6aed53dfb2d17b
Oaketree07 <oakeskyle502@gmail.com>
2025-11-12 10:03:13 +0000

    All logic besides implications and Bi implications have been implemented

--- Project.hs
+++ Project.hs
@@ -1,13 +1,10 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Redundant bracket" #-}
-
 module Project ( prove, proveCount ) where
+import Test.QuickCheck
 import Data.List
 import Sequent
 
-unMaybe :: Maybe [Sequent] -> [Sequent]
-unMaybe (Just xs) = xs
-unMaybe Nothing = []
 
 isAxiom :: Sequent -> Bool
 isAxiom (xs :|=: ss) = any (`elem` ss) xs
@@ -15,36 +12,42 @@ isAxiom (xs :|=: ss) = any (`elem` ss) xs
 lConjunction :: Sequent -> Sequent
 lConjunction (((x :&&: xs) : y) :|=: (sec)) = (x : xs : y) :|=: (sec)           
 lConjunction ((t : (x :&&: xs) : r) :|=: (sec)) = (t : x : xs : r) :|=: (sec)
-lConjunction p = (p)
 
 rConjunction :: Sequent -> [Sequent]
 rConjunction ((fir) :|=: ((x :&&: xs) : y)) = [(fir :|=: (x : y)) , (fir :|=: (xs : y))]
 rConjunction ((fir) :|=: (t : (x :&&: xs) : y)) = [(fir :|=: (t : x : y)) , (fir :|=: (t : xs : y))]
-rConjunction p = [p]
 
 lDisjunction :: Sequent -> [Sequent]
 lDisjunction (((x :||: xs) : y) :|=: (sec)) = [((x : y) :|=: sec),((xs : y):|=: sec)]
 lDisjunction (t : ((x :||: xs) : y) :|=: (sec)) = [((t : x : y) :|=: sec),((t : xs : y):|=: sec)]
-lDisjunction p = [p]
 
 rDisjunction :: Sequent -> Sequent
 rDisjunction ((fir) :|=: ((x :||: xs) : y)) = (fir) :|=: (x : xs : y)
 rDisjunction ((fir) :|=: (t : (x :||: xs) : y)) = (fir) :|=: (t : x : xs : y)
-rDisjunction p = p
 
 negation :: Sequent -> Sequent
 negation (((Not x) : xs) :|=: (sec)) = (xs) :|=: (x : sec)
 negation (((t : (Not x) : xs) :|=: (sec))) = (t : xs) :|=: (x : sec)
 negation ((fir) :|=: ((Not x) : xs)) = (x : fir) :|=: (xs)
 negation ((fir) :|=: (t : (Not x) : xs)) = (x : fir) :|=: (t : xs)
-negation p = p
 
+-- Proof search: returns open leaves; [] means the sequent is proved.
 prove :: Sequent -> [Sequent]
 prove = undefined
 
+bigexample =
+  [ (Var "a" :||: Var "f") :||: (Var "c" :||: Var "d")
+  , (Var "b" :&&: Var "f") :||: (Var "f" :||: Var "a")
+  , (Var "f" :||: Var "f") :&&: Not (Var "f")
+  , Not (Var "a") :||: (Var "c" :||: Var "e")
+  ] :|=: 
+  [ Not (Var "e") :&&: Not (Var "c")
+  , (Var "b" :&&: Var "c") :||: Not (Var "a")
+  , Not (Var "e" :||: Var "d")
+  ]
 
 -- Examples:
-example1 = [] :|=: [ Var "a" :||: Var "b"]
+example1 = [Var "c"] :|=: [ Var "a" :||: Var "b", Var "c"]
 example2 = [ Var "a" :||: Var "b"] :|=: []
 example3 = [] :|=: [ Var "a" :&&: Var "b"]
 example4 = [ Var "a" :&&: Var "b"] :|=: []
